872c10254b003a2f8fad0aaae726d17cf96eb0e6
kirby81 <etienne.ellie@gmail.com>
2016-03-08 22:55:06 -0500

    de la merde

--- INF11207/TP2/source/factory/AccountFactory.cpp
+++ INF11207/TP2/source/factory/AccountFactory.cpp
@@ -3,6 +3,15 @@
 
 Account     *AccountFactory::newObject(const AccountEnum e, const std::string &params)
 {
+    // std::map<std::string, std::string> map;
+    // std::istringstream iss(params);
+    // map["id"] = std::getline(iss, tmp, ',');
+    // for (int i = 0; i < 4; i++);
+    //     std::getline(iss, tmp, ',');
+    // map["idFather"] = std::getline(iss, tmp, ',');
+    // map["balance"] = std::getline(iss, tmp, ',');
+
+
     switch (e) {
         case REGULAR: return new RegularAccount(params);
         case CHILD: return new ChildAccount(params);
